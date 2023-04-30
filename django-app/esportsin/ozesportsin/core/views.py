56a3d5c40dbd4cb633b45ff1f110da89a81e0abe
Bauxitiego <valencia.dieego825@gmail.com>
2023-04-30 17:16:30 +0200

    loaded statics

--- django-app/esportsin/ozesportsin/core/views.py
+++ django-app/esportsin/ozesportsin/core/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Welcome to esportsIn made by Lumina Oz  </h1>')
+    return render(request, 'index.html')
 
 # Create your views here.
